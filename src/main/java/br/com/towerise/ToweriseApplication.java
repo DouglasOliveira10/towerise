4e47e24e1fc7c925f2caaeb03bfd73511b687a31
Douglas Oliveira <douglasig1@hotmail.com>
2023-09-12 11:27:17 -0300

    first commit

---
+++ src/main/java/br/com/towerise/ToweriseApplication.java
@@ -0,0 +1,13 @@
+package br.com.towerise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToweriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToweriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/towerise/ToweriseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.towerise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToweriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
